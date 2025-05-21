f43114038492fa385220fa0f047f6e933e338b8a
canknotcawh <dangconganh999@gmail.com>
2025-05-21 07:58:21 +0700

    Update piecesmanager.h

--- piecesmanager.h
+++ piecesmanager.h
@@ -2,32 +2,32 @@
 #include <string>
 #include <SDL.h>
 #include "pieces.h"
-#include "board.h"
 #include "move.h"
+#include "board.h"
 #include <array>
+#include "menu.h"
 
 class PiecesManager {
 public:
     PiecesManager(SDL_Renderer* p_Renderer, int BoardSize);
-    ~PiecesManager(){}
+    ~PiecesManager();
 
+    std::array<std::array<int, 8>, 8> getBoard() const;
+    std::vector<MoveInfo> moveHistory;
     void drawPieces();
     bool isValidMove(PiecePosition from, PiecePosition to);
     bool movePiece(int fromRow, int fromCol, int toRow, int toCol);
     void promotePawn(int row, int col, bool isWhite);
-    int showPromotionMenu(bool isWhite);
     void resetPieces();
-    void updateBoardPieces(int* p_MouseX,int* p_MouseY);
-    void isPieceSelect(bool p_state,int* p_MouseX,int* p_MouseY);
+    void updateBoardPieces(int* p_MouseX, int* p_MouseY);
+    void isPieceSelect(bool p_state, int* p_MouseX, int* p_MouseY);
 
     void setSize(int p_PieceSize);
     std::string GameOver();
 
-    Player getPlayer() const ;
-    PiecesManager();
-    Move moveManager;
-
     int turn;
+    Player getPlayer() const;
+    Move moveManager;
 
 private:
     void init();
@@ -42,13 +42,11 @@ private:
     bool m_PieceSelectState;
     int m_LastPiece_Row;
     int m_LastPiece_Col;
-    bool m_IsLastBlackMove = 1;
+    bool m_IsLastBlackMove;
     Piece** m_DrawPieces;
     std::array<std::array<int, 8>, 8> m_BoardPieces;
+    Menu menu;
 
     int whiteKingX = -1, whiteKingY = -1;
     int blackKingX = -1, blackKingY = -1;
 };
-
-
-
