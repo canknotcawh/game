46b895e1f8633a1325fe4c20ac8b9305bc7736ca
canknotcawh <dangconganh999@gmail.com>
2025-05-21 07:58:05 +0700

    Update piecesmanager.cpp

--- piecesmanager.cpp
+++ piecesmanager.cpp
@@ -1,6 +1,28 @@
 #include "piecesmanager.h"
-#include "piecesdefs.h"
 #include <iostream>
+
+PiecesManager::PiecesManager(SDL_Renderer* p_Renderer, int BoardSize)
+    : m_Renderer(p_Renderer), m_BoardPieceSize(BoardSize / 8), menu(p_Renderer) {
+
+    Piece::s_Renderer = m_Renderer;
+
+    init();
+    initDefaultBoard();
+    CalculatePieces();
+
+    if (!m_DrawPieces) {
+        m_DrawPieces = new Piece*[8];
+        for (int i = 0; i < 8; ++i) {
+            m_DrawPieces[i] = new Piece[8];
+        }
+    }
+
+    for (int row = 0; row < 8; ++row) {
+        for (int col = 0; col < 8; ++col) {
+            m_DrawPieces[row][col].setPieceSize(m_BoardPieceSize);
+        }
+    }
+}
 
 void PiecesManager::init()
 {
@@ -37,18 +59,6 @@ void PiecesManager::initDefaultBoard()
 bool PiecesManager::isBlack()
 {
     return (m_BoardPieces[m_LastPiece_Row][m_LastPiece_Col] < 0);
-}
-
-PiecesManager::PiecesManager(SDL_Renderer* p_Renderer,int BoardSize)
-    : m_Renderer(p_Renderer), m_BoardPieceSize(BoardSize/8)
-{
-    Piece::s_Renderer = m_Renderer;
-    init();
-    initDefaultBoard();
-    CalculatePieces();
-    for (int row = 0 ; row <= 7 ; row++)
-        for (int col =0 ; col <= 7 ; col++)
-            m_DrawPieces[row][col].setPieceSize(m_BoardPieceSize);
 }
 
 void PiecesManager::addPiece(const char* p_FilePath, int row, int col)
@@ -118,29 +128,43 @@ void PiecesManager::setSize(int p_PieceSize)
 }
 std::string PiecesManager::GameOver()
 {
-    bool WhiteKing = false;
-    bool BlackKing = false;
+    const auto& moveHistory = moveManager.getMoveHistory();
+
+    int whiteKingX = -1, whiteKingY = -1;
+    int blackKingX = -1, blackKingY = -1;
+
     for (int row = 0; row <= 7; row++)
     {
         for (int col = 0; col <= 7; col++)
         {
             if (m_BoardPieces[row][col] == PIECES_TYPE::WHITE_KING)
             {
-                WhiteKing = true;
+                whiteKingX = row;
+                whiteKingY = col;
             }
-            if (m_BoardPieces[row][col] == PIECES_TYPE::BLACK_KING)
+            else if (m_BoardPieces[row][col] == PIECES_TYPE::BLACK_KING)
             {
-                BlackKing = true;
+                blackKingX = row;
+                blackKingY = col;
             }
         }
     }
-    if (!WhiteKing)
-    {
+
+    if (Move::isCheckmate(m_BoardPieces, moveHistory, whiteKingX, whiteKingY, true))
         return "Black";
-    }
-    if (!BlackKing)
-    {
+    if (Move::isCheckmate(m_BoardPieces, moveHistory, blackKingX, blackKingY, false))
         return "White";
+    if (Move::isStalemate(m_BoardPieces, moveHistory, whiteKingX, whiteKingY, true)) {
+        std::cout << "Stalemate! Game is a draw!" << std::endl;
+        return "Draw";
+    }
+    if (Move::isStalemate(m_BoardPieces, moveHistory, blackKingX, blackKingY, false)) {
+        std::cout << "Stalemate! Game is a draw!" << std::endl;
+        return "Draw";
+    }
+    if (Move::isInsufficientMaterial(m_BoardPieces)) {
+        std::cout << "Insufficient Material! It's a Draw!" << std::endl;
+        return "Draw";
     }
     return "";
 }
@@ -160,7 +184,32 @@ bool PiecesManager::movePiece(int fromRow, int fromCol, int toRow, int toCol)
 
     if ((turn == 1 && pieceMoved < 0) || (turn == -1 && pieceMoved > 0))
         return false;
+    if ((pieceMoved == PIECES_TYPE::WHITE_PAWN || pieceMoved == PIECES_TYPE::BLACK_PAWN) &&
+        abs(fromRow - toRow) == 1 && abs(fromCol - toCol) == 1 &&
+        pieceCaptured == PIECES_TYPE::EMPTY) {
 
+        if (Move::canEnPassantLeft(m_BoardPieces, pieceMoved > 0, moveManager.getMoveHistory(), fromRow, fromCol) && toCol == fromCol - 1) {
+            int targetRow = (pieceMoved == PIECES_TYPE::WHITE_PAWN) ? 3 : 4;
+            m_BoardPieces[targetRow][fromCol - 1] = PIECES_TYPE::EMPTY;
+            m_BoardPieces[toRow][toCol] = pieceMoved;
+            m_BoardPieces[fromRow][fromCol] = PIECES_TYPE::EMPTY;
+            moveManager.addMove(fromRow, fromCol, toRow, toCol, pieceMoved, PIECES_TYPE::EMPTY);
+            turn = -turn;
+            CalculatePieces();
+            return true;
+        }
+
+        if (Move::canEnPassantRight(m_BoardPieces, pieceMoved > 0, moveManager.getMoveHistory(), fromRow, fromCol) && toCol == fromCol + 1) {
+            int targetRow = (pieceMoved == PIECES_TYPE::WHITE_PAWN) ? 3 : 4;
+            m_BoardPieces[targetRow][fromCol + 1] = PIECES_TYPE::EMPTY;
+            m_BoardPieces[toRow][toCol] = pieceMoved;
+            m_BoardPieces[fromRow][fromCol] = PIECES_TYPE::EMPTY;
+            moveManager.addMove(fromRow, fromCol, toRow, toCol, pieceMoved, PIECES_TYPE::EMPTY);
+            turn = -turn;
+            CalculatePieces();
+            return true;
+        }
+    }
     if (pieceMoved == PIECES_TYPE::WHITE_KING || pieceMoved == PIECES_TYPE::BLACK_KING)
     {
         if (abs(toCol - fromCol) == 2)
@@ -180,6 +229,16 @@ bool PiecesManager::movePiece(int fromRow, int fromCol, int toRow, int toCol)
                     m_BoardPieces[toRow][toCol + 1] = m_BoardPieces[toRow][toCol - 2];
                     m_BoardPieces[toRow][toCol - 2] = PIECES_TYPE::EMPTY;
                 }
+                if (pieceMoved == PIECES_TYPE::WHITE_KING)
+                {
+                    whiteKingX = toRow;
+                    whiteKingY = toCol;
+                }
+                else if (pieceMoved == PIECES_TYPE::BLACK_KING)
+                {
+                    blackKingX = toRow;
+                    blackKingY = toCol;
+                }
                 moveManager.addMove(fromRow, fromCol, toRow, toCol, pieceMoved, PIECES_TYPE::EMPTY);
                 turn = -turn;
                 CalculatePieces();
@@ -191,27 +250,66 @@ bool PiecesManager::movePiece(int fromRow, int fromCol, int toRow, int toCol)
             }
         }
     }
-    if (pieceMoved == PIECES_TYPE::WHITE_PAWN && toRow == 0)
+
+    if (pieceMoved == PIECES_TYPE::WHITE_KING || pieceMoved == PIECES_TYPE::BLACK_KING)
     {
-        PiecesManager::promotePawn(toRow, toCol, true);
+        if (!Move::ValidMove(m_BoardPieces, PiecePosition(fromRow, fromCol), PiecePosition(toRow, toCol), moveManager.getMoveHistory()))
+            return false;
+
+        int tempPiece = m_BoardPieces[toRow][toCol];
+        m_BoardPieces[toRow][toCol] = pieceMoved;
         m_BoardPieces[fromRow][fromCol] = PIECES_TYPE::EMPTY;
-        moveManager.addMove(fromRow, fromCol, toRow, toCol, pieceMoved, PIECES_TYPE::EMPTY);
-        turn = -turn;
-        CalculatePieces();
-        return true;
-    }
-    else if (pieceMoved == PIECES_TYPE::BLACK_PAWN && toRow == 7)
-    {
-        PiecesManager::promotePawn(toRow, toCol, false);
-        m_BoardPieces[fromRow][fromCol] = PIECES_TYPE::EMPTY;
-        moveManager.addMove(fromRow, fromCol, toRow, toCol, pieceMoved, PIECES_TYPE::EMPTY);
-        turn = -turn;
-        CalculatePieces();
-        return true;
+
+        bool isInCheck = (isWhite) ?
+                         Move::isWhiteKingInCheck(m_BoardPieces, moveManager.getMoveHistory(), toRow, toCol) :
+                         Move::isBlackKingInCheck(m_BoardPieces, moveManager.getMoveHistory(), toRow, toCol);
+
+        if (isInCheck)
+        {
+            m_BoardPieces[fromRow][fromCol] = pieceMoved;
+            m_BoardPieces[toRow][toCol] = tempPiece;
+            return false;
+        }
     }
 
-    if (!Move::ValidMove(m_BoardPieces, PiecePosition(fromRow, fromCol), PiecePosition(toRow, toCol), moveManager.getMoveHistory()))
-        return false;
+    else
+    {
+        if (!Move::ValidMove(m_BoardPieces, PiecePosition(fromRow, fromCol), PiecePosition(toRow, toCol), moveManager.getMoveHistory()))
+            return false;
+
+        int tempPiece = m_BoardPieces[toRow][toCol];
+        m_BoardPieces[toRow][toCol] = pieceMoved;
+        m_BoardPieces[fromRow][fromCol] = PIECES_TYPE::EMPTY;
+
+        bool isInCheck = (isWhite) ?
+                         Move::isWhiteKingInCheck(m_BoardPieces, moveManager.getMoveHistory(), whiteKingX, whiteKingY) :
+                         Move::isBlackKingInCheck(m_BoardPieces, moveManager.getMoveHistory(), blackKingX, blackKingY);
+
+        if (isInCheck)
+        {
+            m_BoardPieces[fromRow][fromCol] = pieceMoved;
+            m_BoardPieces[toRow][toCol] = tempPiece;
+            return false;
+        }
+            if (pieceMoved == PIECES_TYPE::WHITE_PAWN && toRow == 0)
+            {
+                PiecesManager::promotePawn(toRow, toCol, true);
+                m_BoardPieces[fromRow][fromCol] = PIECES_TYPE::EMPTY;
+                moveManager.addMove(fromRow, fromCol, toRow, toCol, pieceMoved, PIECES_TYPE::EMPTY);
+                turn = -turn;
+                CalculatePieces();
+                return true;
+            }
+            else if (pieceMoved == PIECES_TYPE::BLACK_PAWN && toRow == 7)
+            {
+                PiecesManager::promotePawn(toRow, toCol, false);
+                m_BoardPieces[fromRow][fromCol] = PIECES_TYPE::EMPTY;
+                moveManager.addMove(fromRow, fromCol, toRow, toCol, pieceMoved, PIECES_TYPE::EMPTY);
+                turn = -turn;
+                CalculatePieces();
+                return true;
+            }
+    }
 
     m_BoardPieces[toRow][toCol] = pieceMoved;
     m_BoardPieces[fromRow][fromCol] = PIECES_TYPE::EMPTY;
@@ -233,16 +331,18 @@ bool PiecesManager::movePiece(int fromRow, int fromCol, int toRow, int toCol)
     m_LastPiece_Row = toRow;
     m_LastPiece_Col = toCol;
     m_IsLastBlackMove = isBlack();
-
     CalculatePieces();
+    std::string winner = GameOver();
+    if (!winner.empty()) {
+        return false;
+    }
     return true;
 }
 void PiecesManager::promotePawn(int row, int col, bool isWhite)
 {
-    std::cout << "PROMOTION!!!" << std::endl;
-    int choice = showPromotionMenu(isWhite);
-    switch (choice)
-    {
+    int choice = menu.showPromotionMenu(isWhite);
+
+    switch (choice) {
         case 0:
             m_BoardPieces[row][col] = isWhite ? PIECES_TYPE::WHITE_QUEEN : PIECES_TYPE::BLACK_QUEEN;
             break;
@@ -256,17 +356,6 @@ void PiecesManager::promotePawn(int row, int col, bool isWhite)
             m_BoardPieces[row][col] = isWhite ? PIECES_TYPE::WHITE_KNIGHT : PIECES_TYPE::BLACK_KNIGHT;
             break;
     }
-    std::cout << "PROMOTION COMPLETED!!!" << std::endl;
-}
-int PiecesManager::showPromotionMenu(bool isWhite)
-{
-    std::string color = isWhite ? "White" : "Black";
-    std::cout << color << " Pawn Promotion!\n";
-    std::cout << "Choose piece to promote to:\n";
-    std::cout << "0: Queen\n1: Rook\n2: Bishop\n3: Knight\n";
-    int choice;
-    std::cin >> choice;
-    return choice;
 }
 void PiecesManager::resetPieces()
 {
