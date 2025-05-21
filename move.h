b44db5325f5ce3832c52a1d73d9978979410095e
canknotcawh <dangconganh999@gmail.com>
2025-05-21 07:56:27 +0700

    Update move.h

--- move.h
+++ move.h
@@ -20,10 +20,17 @@ public:
 class Move
 {
 public:
-    static bool isBlackKingInCheck(const std::vector<MoveInfo>& moveHistory, const std::array<std::array<int, 8>, 8>& board, int kingX, int kingY);
-    static bool isWhiteKingInCheck(const std::vector<MoveInfo>& moveHistory, const std::array<std::array<int, 8>, 8>& board, int kingX, int kingY);
+    static bool isBlackKingInCheck(const std::array<std::array<int, 8>, 8>& board, const std::vector<MoveInfo>& moveHistory, int kingX, int kingY);
+    static bool isWhiteKingInCheck(const std::array<std::array<int, 8>, 8>& board, const std::vector<MoveInfo>& moveHistory, int kingX, int kingY);
+    static bool isPinned(const std::array<std::array<int, 8>, 8>& board, const std::vector<MoveInfo>& moveHistory, int pieceX, int pieceY);
+    static std::vector<PiecePosition> LegalMoves(const std::array<std::array<int, 8>, 8>& board, const std::vector<MoveInfo>& moveHistory, int kingX, int kingY, int pieceX, int pieceY, bool isWhite);
+    static bool canBlockCheck(const std::vector<MoveInfo>& moveHistory, const std::array<std::array<int, 8>, 8>& board, int kingX, int kingY, int pieceX, int pieceY, bool isWhite);
+    static bool isCheckmate(const std::array<std::array<int, 8>, 8>& board, const std::vector<MoveInfo>& moveHistory, int kingX, int kingY, bool isWhite);
+    static bool isStalemate(const std::array<std::array<int, 8>, 8>& board, const std::vector<MoveInfo>& moveHistory, int kingX, int kingY, bool isWhite);
+    static bool isInsufficientMaterial(const std::array<std::array<int, 8>, 8>& board);
     static bool canCastle(const std::array<std::array<int, 8>, 8>& board, bool isWhite, const std::vector<MoveInfo>& moveHistory);
-    static bool canEnPassant(const std::array<std::array<int, 8>, 8>& board, bool isWhite, const std::vector<MoveInfo>& moveHistory);
+    static bool canEnPassantLeft(const std::array<std::array<int, 8>, 8>& board, bool isWhite, const std::vector<MoveInfo>& moveHistory, int pawnRow, int pawnCol);
+    static bool canEnPassantRight(const std::array<std::array<int, 8>, 8>& board, bool isWhite, const std::vector<MoveInfo>& moveHistory, int pawnRow, int pawnCol);
     static bool ValidMove(const std::array<std::array<int, 8>, 8>& board, PiecePosition from, PiecePosition to, const std::vector<MoveInfo>& moveHistory);
 
     void addMove(int startX, int startY, int endX, int endY, int pieceMoved, int pieceCaptured);
